8b992ba0d56bed96effa42cda4d6f94dacece707
Thomas Wilburn <twilburn@npr.org>
2018-11-29 13:20:43 -0600

    Modularize helpers
    The road to micromodules starts with a single step. This will let us tree-shake the helpers.

--- _base/lib/pym.js
+++ _base/lib/pym.js
@@ -1,3 +1,5 @@
+var { getParameterByName } = require("./helpers");
+
 module.exports = new Promise(ok => {
   var url = "https://pym.nprapps.org/pym.v1.min.js";
   var script = document.createElement("script");
@@ -6,3 +8,16 @@ module.exports = new Promise(ok => {
 
   script.onload = () => ok(new pym.Child());
 });
+
+switch (getParameterByName("mode")) {
+  // Homepage (if someone clicked the "This code will be embedded
+  // on the NPR homepage." checkbox when pulling the embed code.)
+  case "hp":
+  document.body.classList.add("hp");
+  isHomepage = true;
+  break;
+  // Direct links to the child page (iOS app workaround link)
+  case "childlink":
+  document.body.classList.add("childlink");
+  break;
+};
