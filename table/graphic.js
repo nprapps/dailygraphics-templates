2508720f951b91fced2a5c31d077808c2a254978
Thomas Wilburn <twilburn@npr.org>
2019-01-18 11:03:49 -0600

    Simplify table code

--- table/graphic.js
+++ table/graphic.js
@@ -1,48 +1,23 @@
 var pym = require("./lib/pym");
 var ANALYTICS = require("./lib/analytics");
 require("./lib/webfonts");
-var { isMobile } = require("./lib/breakpoints");
-
-// Global vars
-var pymChild = null;
 
 // If sortable:
 // window.Tablesort = require("tablesort");
 // require("tablesort/dist/sorts/tablesort.number.min");
 // Tablesort(document.querySelector("#state-table"))
 
-// Initialize the graphic.
-var onWindowLoaded = function() {
-    render();
+pym.then(child => {
+    pymChild = child;
+    child.sendHeight();
 
-    pym.then(child => {
-        pymChild = child;
-        child.sendHeight();
-
-        pymChild.onMessage("on-screen", function(bucket) {
-            ANALYTICS.trackEvent("on-screen", bucket);
-        });
-        pymChild.onMessage("scroll-depth", function(data) {
-            data = JSON.parse(data);
-            ANALYTICS.trackEvent("scroll-depth", data.percent, data.seconds);
-        });
+    pymChild.onMessage("on-screen", function(bucket) {
+        ANALYTICS.trackEvent("on-screen", bucket);
     });
-    window.addEventListener("resize", render);
-};
+    pymChild.onMessage("scroll-depth", function(data) {
+        data = JSON.parse(data);
+        ANALYTICS.trackEvent("scroll-depth", data.percent, data.seconds);
+    });
 
-// Render the graphic(s). Called by pym with the container width.
-var render = function(containerWidth) {
-	
-    // Render the chart!
-    var container = "#table-graphic";
-    var element = document.querySelector(container);
-    var width = element.offsetWidth;
-
-    // Update iframe
-    if (pymChild) {
-        pymChild.sendHeight();
-    }
-};
-
-//Initially load the graphic
-window.onload = onWindowLoaded;
+    window.addEventListener("resize", () => child.sendHeight());
+});
