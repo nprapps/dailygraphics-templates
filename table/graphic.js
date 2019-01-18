a378df954054b57231b3fda6a4c41bb56091d73a
Thomas Wilburn <twilburn@npr.org>
2019-01-18 12:02:12 -0600

    Bad variable scoping

--- table/graphic.js
+++ table/graphic.js
@@ -8,13 +8,12 @@ require("./lib/webfonts");
 // Tablesort(document.querySelector("#state-table"))
 
 pym.then(child => {
-    pymChild = child;
     child.sendHeight();
 
-    pymChild.onMessage("on-screen", function(bucket) {
+    child.onMessage("on-screen", function(bucket) {
         ANALYTICS.trackEvent("on-screen", bucket);
     });
-    pymChild.onMessage("scroll-depth", function(data) {
+    child.onMessage("scroll-depth", function(data) {
         data = JSON.parse(data);
         ANALYTICS.trackEvent("scroll-depth", data.percent, data.seconds);
     });
