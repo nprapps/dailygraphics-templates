144a186172374dfcfed2a7b001ab2c85324062c0
alykat <ahurt@npr.org>
2019-07-19 18:37:08 -0400

    getAPMonth.js is off by one

--- _base/lib/helpers/getAPMonth.js
+++ _base/lib/helpers/getAPMonth.js
@@ -1,5 +1,5 @@
 module.exports = function(date) {
   var apMonths = [ "Jan.", "Feb.", "March", "April", "May", "June", "July", "Aug.", "Sept.", "Oct.", "Nov.", "Dec." ];
-  var thisMonth = date.getMonth() - 1;
+  var thisMonth = date.getMonth();
   return apMonths[thisMonth];
 };
