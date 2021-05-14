c035cd8f2e684b00cfcae33c57c21bbde903b885
Gabrielle <lamarrlemee@gmail.com>
2021-05-14 09:44:29 -0400

    edits to grouped bar chart template

--- _base/lib/webfonts.js
+++ _base/lib/webfonts.js
@@ -5,8 +5,8 @@ document.head.appendChild(script);
 script.onload = function() {
   WebFont.load({
       google: {
-        families: ['Barlow Condensed:Semi-Bold,Semi-Bold Italic',
-                   'IBM Plex Serif:Regular,Regular Italic,Bold,Bold Italic']
+        families: ['Barlow Condensed:Semi-Bold,Regular',
+                   'IBM Plex Serif:Regular,Regular Italic']
      },
       timeout: 10000
   });
