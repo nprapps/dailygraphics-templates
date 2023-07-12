84a9ef1c56dc8b12f52bc32f95a62a8ed2427475
Alyson Hurt <alykat@gmail.com>
2023-07-12 17:03:50 -0400

    dot_chart_multiple: rework how the legend is set up

--- dot_chart_multiple/graphic.js
+++ dot_chart_multiple/graphic.js
@@ -7,62 +7,62 @@ var pymChild = null;
 var renderDotChart = require("./renderDotChart");
 
 // Initialize the graphic.
-var onWindowLoaded = function() {
-    if (document.body.classList.contains('promo')) {
-        isPromo = true;
-    }
+var onWindowLoaded = function () {
+  if (document.body.classList.contains('promo')) {
+    isPromo = true;
+  }
 
-    render(window.DATA);
-    window.addEventListener("resize", () => render(window.DATA));
+  render(window.DATA);
+  window.addEventListener("resize", () => render(window.DATA));
 
-    pym.then(child => {
-        pymChild = child;
-        pymChild.sendHeight();
-    });
+  pym.then(child => {
+    pymChild = child;
+    pymChild.sendHeight();
+  });
 };
 
 // Render the graphic(s). Called by pym with the container width.
-var render = function(data) {
-// Render the chart!
-var container = "#dot-chart";
-var element = document.querySelector(container);
-element.innerHTML = "";
-var width = element.offsetWidth;
+var render = function (data) {
+  // Render the chart!
+  var container = "#dot-chart";
+  var element = document.querySelector(container);
+  element.innerHTML = "";
+  var width = element.offsetWidth;
 
-// draw the charts
-if (isMobile.matches) {
+  // draw the charts
+  if (isMobile.matches) {
     for (var i = 0; i < data.length; i++) {
-        var chartDiv = document.createElement('div');
-        chartDiv.className = 'chart chart-' + i;
-        element.appendChild(chartDiv);
+      var chartDiv = document.createElement('div');
+      chartDiv.className = 'chart chart-' + i;
+      element.appendChild(chartDiv);
 
-    // Render the chart!
-    renderDotChart({
+      // Render the chart!
+      renderDotChart({
         container: container + ' .chart-' + i,
         width,
-        data: [ data[i] ],
+        data: [data[i]],
         idx: i,
         labelColumn: "label",
         minColumn: "min",
         maxColumn: "max"
-    });
+      });
     }
-} else {
+  } else {
     // Render the chart!
     renderDotChart({
-    container,
-    width,
-    data,
-    labelColumn: "label_fmt",
-    minColumn: "min",
-    maxColumn: "max"
+      container,
+      width,
+      data,
+      labelColumn: "label_fmt",
+      minColumn: "min",
+      maxColumn: "max"
     });
-}
+  }
 
-// Update iframe
-if (pymChild) {
+  // Update iframe
+  if (pymChild) {
     pymChild.sendHeight();
-}
+  }
 };
 
 /*
