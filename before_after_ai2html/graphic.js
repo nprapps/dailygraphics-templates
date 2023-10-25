a0e994f3839a17b370185f95fd826516dde02b9a
Daniel Wood <daniel.j.woo11@gmail.com>
2023-10-25 11:56:25 -0400

    add before_after_ai2html template

---
+++ before_after_ai2html/graphic.js
@@ -0,0 +1,121 @@
+var pym = require("./lib/pym");
+var ANALYTICS = require("./lib/analytics");
+require("./lib/webfonts");
+var { isMobile } = require("./lib/breakpoints");
+
+var pymChild = null;
+var isMobile = isMobile.matches;
+var hasStarted = false;
+
+var onWindowLoaded = function() {
+  pym.then(child => {
+    pymChild = child;
+    child.sendHeight();
+    window.addEventListener("resize", render);
+  });
+
+  initUI();
+}
+
+/*
+ * Render the graphic.
+ */
+var render = function() {
+  // Update iframe
+  if (pymChild) {
+    pymChild.sendHeight();
+  }
+}
+
+var initUI = function() {
+  document.querySelectorAll('.graphic').forEach(function(wrapper) {
+    var toggleTimeout;
+    var toggleButtons = wrapper.querySelectorAll('.toggle-btn');
+    var splitId = toggleButtons[0].getAttribute('id').split('-');    
+
+    toggleButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        var parentToggleWrap = button.parentElement;
+        parentToggleWrap.classList.add('clicked');
+        window.clearTimeout(toggleTimeout);
+
+        splitId = button.getAttribute('id').split('-');
+
+        // select before image by its unique id
+        // var beforeImages = wrapper.querySelector('#image-' + splitId[1] + '-1');
+        var beforeImages = wrapper.querySelectorAll('[id$="before"]');
+
+        if (splitId[2] == '1') {
+          beforeImages.forEach(el => el.classList.remove('hidden'))
+          // beforeImage.classList.remove('hidden');
+        } else {
+          beforeImages.forEach(el => el.classList.add('hidden'))
+          // beforeImage.classList.add('hidden');
+        }
+        
+        if (!button.classList.contains('active')) {
+          let activeToggleBtn = parentToggleWrap.querySelector('.toggle-btn.active');
+          if (activeToggleBtn) {
+            activeToggleBtn.classList.remove('active');
+          }
+          button.classList.add('active');
+        }
+      });
+    });
+
+    var autoToggle = function() {
+      var stepList = [2, 1, 2, 1, 2];
+      toggleStep(0);
+
+      function toggleStep(step_i) {
+        if (step_i < stepList.length) {
+          var step = stepList[step_i];
+
+          if (!wrapper.querySelector('.image-toggle').classList.contains('clicked')) {
+            // select before image by its unique id
+            // var beforeImage = wrapper.querySelector('#image-' + splitId[1] + '-1');
+            var beforeImages = wrapper.querySelectorAll('[id$="before"]');
+
+            if (step == 1) {
+              beforeImages.forEach(el => el.classList.remove('hidden'))
+            } else {
+              beforeImages.forEach(el => el.classList.add('hidden'))
+            }
+
+            let activeToggleBtn = wrapper.querySelector('.toggle-btn.active');
+            if (activeToggleBtn) {
+                activeToggleBtn.classList.remove('active');
+            }
+            
+            wrapper.querySelector('#toggle-' + splitId[1] + '-' + step).classList.add('active');
+            
+            toggleTimeout = window.setTimeout(toggleStep, 2000, step_i + 1);
+          }
+        }
+      }
+    };
+
+    //start looping through frames once graphic is visible
+    var observer = new IntersectionObserver(intersectionCallback, {threshold: 0.25});
+    var target = document.querySelector(".target");
+    observer.observe(target);
+    
+    function intersectionCallback(entries, observer){
+      entries.forEach(entry=> {
+        if(entry.isIntersecting){
+          //only start animation once
+          if(hasStarted) return;
+
+          console.log('here!~~!~')
+          autoToggle();
+          hasStarted = true;
+        }
+      })
+    }
+  });
+};
+/*
+ * Initially load the graphic
+ * (NB: Use window.load to ensure all images have loaded)
+ */
+window.onload = onWindowLoaded;
