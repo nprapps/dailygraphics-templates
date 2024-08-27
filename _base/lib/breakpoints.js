383efb8700144dab8fa70861ca03a109b85b32a2
Hilary Fung <1105386+hilaryfung@users.noreply.github.com>
2024-08-27 11:30:07 -0700

    add touchscreen/hover detection

--- _base/lib/breakpoints.js
+++ _base/lib/breakpoints.js
@@ -1,4 +1,6 @@
 module.exports = {
   isMobile: window.matchMedia("(max-width: 500px)"),
-  isDesktop: window.matchMedia("(min-width: 501px)")
+  isDesktop: window.matchMedia("(min-width: 501px)"),
+  hasTouchscreen: window.matchMedia("(hover: none)"),
+  hasHover: window.matchMedia("(hover: hover) and (pointer: fine)")
 };
