e95ed67086fde25e51b968a503f45b7726cafd8b
Hilary Fung <1105386+hilaryfung@users.noreply.github.com>
2024-08-28 08:44:49 -0700

    add coarse pointer detection for samsung devices

--- _base/lib/breakpoints.js
+++ _base/lib/breakpoints.js
@@ -1,6 +1,6 @@
 module.exports = {
   isMobile: window.matchMedia("(max-width: 500px)"),
   isDesktop: window.matchMedia("(min-width: 501px)"),
-  hasTouchscreen: window.matchMedia("(hover: none)"),
+  hasTouchscreen: window.matchMedia("(hover: none), (pointer: coarse)"),
   hasHover: window.matchMedia("(hover: hover) and (pointer: fine)")
 };
